06aed620505dc30c266dbabf3db7a829a94da111
ArsenyoP <arsenyo198510@gmail.com>
2026-09-07 22:28:53 +0300

    Added base URL for local frontend

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios'
+
+axios.defaults.baseURL="http://localhost:8080/api/"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
