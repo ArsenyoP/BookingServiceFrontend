d6b410e71bfa25d7292593104ea26dc978d8ae3d
ArsenyoP <arsenyo198510@gmail.com>
2026-09-07 22:23:31 +0300

    Created Header component

---
+++ src/Components/Header.tsx
@@ -0,0 +1,15 @@
+export const Header = () => {
+    return <header>
+    <div className="container">
+      <h1 className="logo">BookingService</h1>
+      <nav>
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="listing.html">Listings</a></li>
+          <li><a href="login.html">Login</a></li>
+          <li><a href="register.html">Register</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+}
