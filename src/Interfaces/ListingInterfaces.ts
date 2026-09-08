7fd8384ffd9ea532ba41de24126da52c98910bd4
ArsenyoP <arsenyo198510@gmail.com>
2026-09-08 14:47:14 +0300

    Fixed listing interface

---
+++ src/Interfaces/ListingInterfaces.ts
@@ -0,0 +1,11 @@
+export interface ListingResponseInterface{
+    id: string;
+    title: string;
+    description: string;
+    address: string;
+    city: string;
+    country: string;
+    pricePerNight: number;
+    maxGuests: number;
+    averageRating: number;
+}
