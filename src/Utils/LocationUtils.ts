9e1c18ce697661c456b7401c5235f8aa1cf45b83
ArsenyoP <arsenyo198510@gmail.com>
2026-09-10 23:17:57 +0300

    Overloaded function formatLocation

--- src/Utils/LocationUtils.ts
+++ src/Utils/LocationUtils.ts
@@ -1,3 +1,14 @@
-export const formatLocation = (country: string, city: string): string => {
-    return `${country}, ${city}`
+export function formatLocation(country: string, city: string): string;
+export function formatLocation(country: string, city: string, street: string, houseNumber: string | number): string;
+
+export function formatLocation(
+  country: string, 
+  city: string, 
+  street?: string, 
+  houseNumber?: string | number
+): string {
+  if (street && houseNumber) {
+    return `${country}, ${city}, вул. ${street} ${houseNumber}`;
+  }
+  return `${country}, ${city}`;
 }
