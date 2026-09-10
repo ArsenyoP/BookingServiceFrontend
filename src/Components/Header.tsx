5b5c63cda062910f63e62a9d9a56daf6bee0eca5
ArsenyoP <arsenyo198510@gmail.com>
2026-09-10 21:00:55 +0300

    Fixed reference to home page in header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
     </a>
       <nav>
         <ul>
-          <li><a href="index.html">Home</a></li>
+          <li><a href="/">Home</a></li>
           <li><a href="listing.html">Listings</a></li>
           <li><a href="login.html">Login</a></li>
           <li><a href="register.html">Register</a></li>
