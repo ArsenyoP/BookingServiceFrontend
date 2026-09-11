286f5de119a76d4af686df1eb470439fa11edec0
ArsenyoP <arsenyo198510@gmail.com>
2026-09-11 22:35:54 +0300

    Created ReviewInterface

---
+++ src/Interfaces/ReviewsInterfaces/ReviewInterface.ts
@@ -0,0 +1,9 @@
+export interface ReviewInterface{
+    score: number;
+    text: string;
+    userName: string;
+    targetType: string;
+    targetTitle: string;
+    createdAt: string;
+    isEdited: boolean;
+}
